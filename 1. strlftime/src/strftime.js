88a3a8e8a32df9d21531b6990103ef5fe759e811
Robert Freire <robert.freire@gmail.com>
2015-11-09 09:50:26 +0100

    Fix padding in year

--- 1. strlftime/src/strftime.js
+++ 1. strlftime/src/strftime.js
@@ -31,7 +31,7 @@ Date.prototype.strftime = (function () {
     },
 
     y: function (date) {
-      return date.getYear() % 100;
+      return zeroPad(date.getYear() % 100);
     },
 
     Y: function (date) {
