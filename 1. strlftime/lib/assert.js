214f0ddcf1c55a1934f326a9e42dadab78546ef8
Robert Freire <robert.freire@gmail.com>
2015-11-29 09:54:30 +0100

    TestSuite and describe

--- 1. strlftime/lib/assert.js
+++ 1. strlftime/lib/assert.js
@@ -1,18 +1,44 @@
 function assert(message, expr) {
-  if (!expr) {
-    throw new Error(message);
-  }
-
-  assert.count++;
-
-  return true;
+	this.asserts++;
+	if (!expr) {
+	    throw new Error(message);
+	}
+	this.successful++;
+  	return true;
 }
 
-assert.count = 0;
 
 function output(text, color) {
   var p = document.createElement("p");
   p.innerHTML = text;
   p.style.color = color;
   document.body.appendChild(p);
+}
+
+function describe(name, test){
+	this.asserts = 0;
+	this.successful = 0;
+
+	if (typeof this.testSetup == "function"){
+		testSetup.apply(this);
+	}
+
+	try{
+		test.apply(this);
+		output (name+ " test OK ", "#0c0");
+
+	} catch (e) {
+	    output ("Test failed: " + e.message, "#c00");
+	}
+
+	var color = this.successful == this.asserts ? "#0c0": "#c00";
+	output("<strong>" + this.asserts  + " tests, " + (this.asserts  - this.successful) + " failures</strong>", color);
+}
+
+function testSetup(setup){
+	this.testSetup = setup;
+}
+
+function testSuite (text, test){
+	test.apply(this);
 }
--- 1. strlftime/test/strftime_test.js
+++ 1. strlftime/test/strftime_test.js
@@ -1,21 +1,34 @@
-  var date = new Date(2009, 9, 2, 22, 14, 45);
+  
 
-  try {
-    
-    assert("%Y should return full year",
-      Date.formats.Y(this.date) === 2009);
-    assert("%m should return month",
+
+testSuite ("format dates", function () {
+
+  testSetup (function(){
+    this.date = new Date(2009, 9, 2, 22, 14, 45);
+  });
+
+  describe ("test format specifier %Y", function (){
+     assert("%Y should return full year",
+       Date.formats.Y(this.date) === 2009);
+  });
+
+  describe ("test format specifier %m", function (){
+     assert("%m should return month",
        Date.formats.m(this.date) === "10");
-    assert("%d should return date",
-           Date.formats.d(this.date) === "02");
-    assert("%y should return year as two digits",
-           Date.formats.y(this.date) === "09");
-    assert("%F should be shortcut for %Y-%m-%d",
-           Date.formats.F === "%Y-%m-%d");
+  });
 
-    output (assert.count + " test OK ", "#0c0");
-  } catch (e) {
-    output ("Test failed: " + e.message, "#c00");
-  }
+  describe ("test format specifier %d", function (){
+     assert("%d should return date",
+       Date.formats.d(this.date) === "02");
+  });
 
+  describe ("test format specifier y", function (){
+     assert("%y should return year as two digits",
+       Date.formats.y(this.date) === "09");
+  });
+  describe ("test format specifier %Y", function (){
+     assert("%F should act as %Y-%m-%d",
+       Date.formats.F === "%Y-%m-%d");
+  });
+});
 
